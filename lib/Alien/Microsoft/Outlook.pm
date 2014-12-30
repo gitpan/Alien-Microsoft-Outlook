97707508f8ff73c07f52433bfebd00387ef7fb47
Duncan Garland <duncan@cpan.org>
2014-12-30 12:43:51 +0000

    Import of DUNCAN/Alien-Microsoft-Outlook-0.04.tar.gz from CPAN.
    gitpan-cpan-distribution: Alien-Microsoft-Outlook
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         DUNCAN/Alien-Microsoft-Outlook-0.04.tar.gz
    gitpan-cpan-author:       DUNCAN
    gitpan-cpan-maturity:     released

--- lib/Alien/Microsoft/Outlook.pm
+++ lib/Alien/Microsoft/Outlook.pm
@@ -14,11 +14,11 @@ Alien::Microsoft::Outlook
 
 =head1 VERSION
 
-Version 0.03_002
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03_002';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
--- tmp.pl
+++
@@ -1,6 +0,0 @@
-use strict;
-use warnings;
-use Test::Kwalitee;
-
-$DB::single=1;
-Test::Kwalitee->import("use_strict");
